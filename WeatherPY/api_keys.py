e66ca94ddad5b64cc87aaf39e066aa94d9d83d68
PapaKLK <kjkrause1323@gmail.com>
2023-05-08 11:25:49 -0500

    Update Starter

--- WeatherPY/api_keys.py
+++ WeatherPY/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "414444a33e95d696d68619e440ce1123"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "631fe2347ddd4c938d49704a4c6fb288"
